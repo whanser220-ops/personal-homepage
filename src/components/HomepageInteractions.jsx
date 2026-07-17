449e76014b4cf2181bff18e16e3e8a46213451c9
whanser220-ops <246406020+whanser220-ops@users.noreply.github.com>
2026-07-17 14:51:15 +0800

    Optimize homepage static delivery

---
+++ src/components/HomepageInteractions.jsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { useHomepageInteractions } from "../hooks/useHomepageInteractions.js";
+
+export function HomepageInteractions() {
+  useHomepageInteractions();
+  return null;
+}
